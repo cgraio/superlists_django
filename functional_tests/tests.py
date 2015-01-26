3c49adbf9e17017749283299cd5a38b33e208ab7
Claudio <claudio.cuervo.17@gmail.com>
2015-01-26 19:11:45 -0300

    Primeros pasos en Test Funcional para testear el layout y el estilo

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.keys import Keys
 class NewVisitorTest(LiveServerTestCase):
 	def setUp(self):
 		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(3)
+		self.browser.implicitly_wait(5)
 
 	def tearDown(self):
 		self.browser.quit()
@@ -81,4 +81,25 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertIn('Buy milk', page_text)
 		# Satisfied, they both go back to sleep
 
-		self.fail('Finish the test.')
+	def test_layout_and_styling(self):
+		# Edith goes to the home_page
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
+
+		# She notices the input is nicely centered
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=5
+		)
+
+		# She starts a new list and sees the input is nicely
+		# centered there too
+		inputbox.send_keys('testing\n')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=5
+		)
